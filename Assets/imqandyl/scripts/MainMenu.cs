ae12a787089ecf49a28f06026e7ac978c811c8ac
imane-qandyl <imaneqandil@gmail.com>
2026-01-10 17:04:37 +0400

    add the video but not working

--- Assets/imqandyl/scripts/MainMenu.cs
+++ Assets/imqandyl/scripts/MainMenu.cs
@@ -7,25 +7,26 @@ public class MainMenu : MonoBehaviour
     public VideoPlayer introVideo;
     public AudioSource menuMusic;
 
-    public void PlayGame()
-    {
-        // Stop menu music
-        if (menuMusic != null)
-            menuMusic.Stop();
+   public void PlayGame()
+{
+    Debug.Log("PLAY BUTTON CLICKED");
 
-        // Play intro video
-        if (introVideo != null)
-        {
-            introVideo.gameObject.SetActive(true);
-            introVideo.Play();
-            introVideo.loopPointReached += OnVideoFinished;
-        }
-        else
-        {
-            // Fallback if video missing
-            SceneManager.LoadScene("GameScene");
-        }
+    if (menuMusic != null)
+        menuMusic.Stop();
+
+    if (introVideo != null)
+    {
+        Debug.Log("VIDEO FOUND, PLAYING");
+        introVideo.gameObject.SetActive(true);
+        introVideo.Play();
+        introVideo.loopPointReached += OnVideoFinished;
     }
+    else
+    {
+        Debug.Log("NO VIDEO, LOADING SCENE");
+        SceneManager.LoadScene("GameScene");
+    }
+}
 
     void OnVideoFinished(VideoPlayer vp)
     {
